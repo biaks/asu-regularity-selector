a44630f338ff307f44cf176f5e9d2a45ce485cfb
ya.kryukov <ya.kryukov@rc.center>
2018-04-04 00:35:16 +0300

    Переделал работу с тремя кнопками: ПО ЧЕТНЫМ, ПО НЕЧЕТНЫМ, ЕЖЕДНЕВНО

--- lib/src/regularityselector.component.ts
+++ lib/src/regularityselector.component.ts
@@ -12,6 +12,8 @@ export class RegularitySelectorComponent {
 
   selected_week_days:  boolean[] = [];
   selected_month_days: boolean[] = [];
+  even_is_selected: boolean;
+  odd_is_selected: boolean;
 
   month_days_matrix: number[][] = Array<number>(5).fill(0).map( (value1, index1) => {
     return Array<number>(7).fill(0).map( (value2, index2) => {
@@ -23,7 +25,10 @@ export class RegularitySelectorComponent {
   // от 10 до 40 - это выбранные дни месяца с 1 по 31
   // например value: number[] = [2, 5, 18, 34]; 
   @Input() set value(value: number[]) {
-    
+
+  this.even_is_selected = false;
+  this.odd_is_selected = false;
+
     if (Array.isArray(value)) {
 
       value.forEach( value => {
@@ -33,7 +38,13 @@ export class RegularitySelectorComponent {
         if (9 < value && value < 41)
           this.selected_month_days[value - (9 +0)] = true;
         
-        // чтобы смотреть лог
+        if (value == 41)
+          this.even_is_selected = true;
+
+        if (value == 42)
+          this.odd_is_selected = true;
+
+          // чтобы смотреть лог
         //if (1 < value && value < 9)
         //  console.log("found", value, "set", value - (1 +1));
         //if (9 < value && value < 41)
@@ -47,7 +58,7 @@ export class RegularitySelectorComponent {
       this.selected_month_days = [];
     }
       
-    
+  
   }
   
   @Output() valueChange: EventEmitter<number[]> = new EventEmitter<number[]>();
@@ -62,6 +73,11 @@ export class RegularitySelectorComponent {
     this.selected_month_days.forEach( (value, index) => {
       result.push(index + (9 +0));
     })
+
+    if (this.even_is_selected)
+      result.push(41);
+    if (this.odd_is_selected)
+      result.push(42);
 
     // чтобы смотреть лог
     //this.selected_week_days.forEach( (value, index) => {
@@ -80,6 +96,51 @@ export class RegularitySelectorComponent {
     this.onMouseLeave.emit(this.value);
   }
 
+  isEveryDaySelected(): boolean {
+    if (this.even_is_selected)
+      return false;
+    if (this.odd_is_selected)
+      return false;
+    if (this.selected_week_days.length > 0)
+      return false;
+    if (this.selected_month_days.length > 0)
+      return false;
+    return true;
+  }
+
+  setEveryDays() {
+    this.even_is_selected = false;
+    this.odd_is_selected = false;
+
+    this.selected_week_days = [];
+    this.selected_month_days = [];
+
+    this.valueChange.emit(this.value);
+  }
+
+  setEvenDays() {
+    if (this.even_is_selected) {
+      this.even_is_selected = false;
+    }
+    else {
+      this.even_is_selected = true;
+      this.odd_is_selected  = false;
+    }
+    this.valueChange.emit(this.value);
+  }
+
+  setOddDays() {
+    if (this.odd_is_selected) {
+      this.odd_is_selected = false;
+    }
+    else {
+      this.odd_is_selected  = true;
+      this.even_is_selected = false;
+    }
+    this.valueChange.emit(this.value);
+  }
+
+/*
   // even - четные
   allEvenDaysIsSelected(): boolean {
     for (let i=2; i<=30; i+=2)
@@ -151,6 +212,7 @@ export class RegularitySelectorComponent {
   allMonthDaysIsUnselected(): boolean {
     return this.allEvenDaysIsUnselected() && this.allOddDaysIsUnselected();
   }
+*/
 
   isSelectedWeekDay(day: number): boolean {
     return (this.selected_week_days[day] == true);
